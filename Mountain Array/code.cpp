eb4dc7e60640cd396059093aba68625d6f282db0
parvbilla <parvbilla2004@gmail.com>
2026-08-08 15:24:01 +0530

    Add Peak Index in Mountain Array Solution

---
+++ Mountain Array/code.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int peakIndex(vector<int> arr){
+    int st=1 , end=arr.size()-2;
+    while(st<=end){
+        int mid = st + (end-st)/2;
+        
+        if(arr[mid-1]<arr[mid] && arr[mid]>arr[mid+1]){
+            
+            return mid;
+        }else if (arr[mid-1]<arr[mid]){
+            st=mid+1;
+        }else{
+            end=mid-1;
+        }
+    }
+    
+    return -1;
+}
+int main(){
+    vector<int> arr = {0,3,8,9,5,2};
+    int index = peakIndex(arr);
+    cout<<"index :- "<<index;
+}
