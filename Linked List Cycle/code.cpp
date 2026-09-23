46321d7949f4385160fb00f8ffcfea19d0148ede
parvbilla <parvbilla2004@gmail.com>
2026-09-23 09:47:48 +0530

    Add Linked list Cycle Solution

---
+++ Linked List Cycle/code.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = nullptr;
+    }
+};
+
+bool hasCycle(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    ListNode* head = new ListNode(3);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(0);
+    head->next->next->next = new ListNode(-4);
+
+    head->next->next->next->next = head->next;
+
+    if (hasCycle(head)) {
+        cout << "Cycle exists" << endl;
+    } else {
+        cout << "No cycle" << endl;
+    }
+
+    return 0;
+}
