b41175c0282b825c42c764a1cb905ffd6e434988
parvbilla <parvbilla2004@gmail.com>
2026-09-23 11:46:10 +0530

    Add Middle of the Linked List Solution

---
+++ Middle of the Linked List/code.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = nullptr;
+    }
+};
+
+ListNode* middleOfLinkedList(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    ListNode* middle = middleOfLinkedList(head);
+
+    cout << "Middle: " << middle->val << endl;
+
+    return 0;
+}
