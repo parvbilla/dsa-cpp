9be603dcfa49bb1643e91a37b86b626becbaf839
parvbilla <parvbilla2004@gmail.com>
2026-09-23 11:41:44 +0530

    Add Linked List Cycle II Solution

---
+++ Linked List Cycle II/code.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = nullptr;
+    }
+};
+
+ListNode* startingPointCycle(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            slow = head;
+
+            while (slow != fast) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+
+            return slow;
+        }
+    }
+
+    return nullptr;
+}
+
+int main() {
+    ListNode* head = new ListNode(3);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(0);
+    head->next->next->next = new ListNode(-4);
+
+    head->next->next->next->next = head->next;
+
+    ListNode* result = startingPointCycle(head);
+
+    if (result != nullptr) {
+        cout << "Cycle starts at: " << result->val << endl;
+    } else {
+        cout << "No cycle" << endl;
+    }
+
+    return 0;
+}
